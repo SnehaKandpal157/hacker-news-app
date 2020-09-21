2321246a6306f7041a6d185444484a7fae06fe50
Sneha <snehakandpal157@gmail.com>
2020-09-21 12:58:59 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NewsComponent from "./components/NewsComponent";
+import "../src/style.css";
+function App() {
+  return (
+    <div className="App">
+      <NewsComponent/>
+    </div>
+  );
+}
+
+export default App;
